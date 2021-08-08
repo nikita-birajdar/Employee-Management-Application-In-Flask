11de651322693750086a0e941ab85562128be719
nikita-birajdar <nikki11birajdar@gmail.com>
2021-08-09 00:12:55 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask.helpers import url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "Secret Key"
+
+app.config['SQLALCHEMY_DATABASE_URI'] ="sqlite:///employee.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def __init__(self, name, email,phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+    
+
+
+@app.route('/')
+def Index():
+    all_data = Employee.query.all()
+    return render_template("index.html", employees = all_data)
+    # return "Hello, Flask!"
+
+@app.route('/insert',methods = ['POST'])
+def insert():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        my_data =Employee(name, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+
+        flash("Employee Inserted Successfully")
+        
+        return redirect(url_for('Index'))
+
+@app.route('/update',methods=['GET','POST'])
+def update():
+   if request.method == 'POST':
+        my_data = Employee.query.get(request.form.get('id'))
+
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.phone = request.form['phone']
+
+        db.session.commit()
+        flash("Employee Data Updated Successfully")
+        
+        return redirect(url_for('Index'))
+
+
+@app.route('/delete/<id>/',methods = ['GET','POST'])
+def delete(id):
+    my_data = Employee.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Employee Data Deleted Successfully")
+
+    return redirect(url_for('Index'))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
